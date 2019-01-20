b577a9bacb3057c72ad4787072a3434d848a6778
johnshaughnessy <johnfshaughnessy@gmail.com>
2019-01-19 17:53:29 -0800

    Squish

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+function lerp(a, b, t) {
+  return (1 - t) * a + b * t;
+}
 const keyboard = {};
 window.addEventListener("keydown", e => {
   keyboard[e.key] = true;
@@ -20,6 +23,10 @@ loader.onProgress.add(() => {}); // called once per loaded/errored file
 loader.onError.add(() => {}); // called once per errored file
 loader.onLoad.add(() => {}); // called once per loaded file
 
+loader.load((loader, resources) => {
+  sprites.drop = new PIXI.Sprite(resources.drop.texture);
+});
+
 function startGame() {
   app.stage.addChild(sprites.drop);
   sprites.drop.anchor.set(0.5);
@@ -36,11 +43,17 @@ function startGame() {
     if (keyboard["ArrowRight"]) {
       sprites.drop.position.x += speed * delta;
     }
+    const squeezed = 0.8;
+    const long = 1.15;
+    if (keyboard["ArrowDown"]) {
+      sprites.drop.scale.x = lerp(sprites.drop.scale.x, squeezed, 0.3);
+      sprites.drop.scale.y = lerp(sprites.drop.scale.y, long, 0.3);
+      sprites.drop.position.y += speed * delta;
+    } else {
+      sprites.drop.scale.x = lerp(sprites.drop.scale.x, 1, 0.3);
+      sprites.drop.scale.y = lerp(sprites.drop.scale.y, 1, 0.3);
+    }
   });
 }
 
 loader.onComplete.add(startGame); // called once when the queued resources all load.
-
-loader.load((loader, resources) => {
-    sprites.drop = new PIXI.Sprite(resources.drop.texture);
-});
