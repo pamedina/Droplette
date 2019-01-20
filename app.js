01dda6bc43b0f8a7f623571872437d5ab1356def
johnshaughnessy <johnfshaughnessy@gmail.com>
2019-01-19 16:58:44 -0800

    Use PIXI loader

--- app.js
+++ app.js
@@ -4,31 +4,28 @@ var app = new PIXI.Application(window.innerWidth, window.innerHeight, {
 
 document.body.appendChild(app.view);
 
-// create a new Sprite from anchor image path
+const loader = PIXI.loader;
+const sprites = {};
+loader.add("drop", "droplette-faded.png");
 
-var bunny = PIXI.Sprite.fromImage("droplette-faded.png");
-// var scaleX = 0.1;
-// var scaleY = 0.1;
-// bunny.scale.set(scaleX , scaleY );
-console.log(bunny);
+loader.onProgress.add(() => {}); // called once per loaded/errored file
+loader.onError.add(() => {}); // called once per errored file
+loader.onLoad.add(() => {}); // called once per loaded file
 
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
-
-// // move the sprite to the center of the screen
-// bunny.x = app.screen.width / 2;
-// bunny.y = bunny.height;
-
-app.stage.addChild(bunny);
-// move the sprite to the center of the screen
-bunny.x = app.screen.width / 2;
-bunny.y = 0;
-
-// Listen for animate update
-app.ticker.add(function(delta) {
-  // just for fun, let's rotate mr rabbit a little
-  // delta is 1 if running at 100% performance
-  // creates frame-independent transformation
-  bunny.rotation += 0.001 * delta;
-  bunny.position.y += 1 * delta;
+loader.load((loader, resources) => {
+  sprites.drop = new PIXI.Sprite(resources.drop.texture);
 });
+
+function startGame() {
+  app.stage.addChild(sprites.drop);
+  sprites.drop.anchor.set(0.5);
+  sprites.drop.x = app.screen.width / 2;
+  sprites.drop.y = sprites.drop.height / 2;
+
+  app.ticker.add(function(delta) {
+    sprites.drop.rotation += 0.001 * delta;
+    sprites.drop.position.y += 1 * delta;
+  });
+}
+
+loader.onComplete.add(startGame); // called once when the queued resources all load.
